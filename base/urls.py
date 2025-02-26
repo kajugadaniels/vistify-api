4836a7878aa59867a94d8850cc5ae6a7bc7a2591
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-26 22:35:19 +0200

    Add protected CRUD routes for Place with nested images and social medias

--- base/urls.py
+++ base/urls.py
@@ -7,4 +7,10 @@ app_name = 'base'
 
 urlpatterns = [
     path('rwanda-locations/', getRwandaLocations, name='getRwandaLocations'),
+
+    path('places/', getPlaces, name='getPlaces'),
+    path('places/add/', addPlace, name='addPlace'),
+    path('places/<int:pk>/', placeDetails, name='placeDetails'),
+    path('places/<int:pk>/edit/', editPlace, name='editPlace'),
+    path('places/<int:pk>/delete/', deletePlace, name='deletePlace'),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
