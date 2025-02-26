9688797e675dc2680ba9b75cd97644506581696c
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-26 22:50:26 +0200

    deletePlaceSocialMedia function to delete place socials

--- base/views.py
+++ base/views.py
@@ -193,3 +193,13 @@ def editPlaceSocialMedia(request, pk):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def deletePlaceSocialMedia(request, pk):
+    try:
+        social = PlaceSocialMedia.objects.get(pk=pk)
+    except PlaceSocialMedia.DoesNotExist:
+        return Response({"detail": "PlaceSocialMedia not found."}, status=status.HTTP_404_NOT_FOUND)
+    social.delete()
+    return Response({"detail": "PlaceSocialMedia deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
