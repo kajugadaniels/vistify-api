c6e6f31bfdf47ef8785e3284db967ec62c5680b2
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-26 22:54:14 +0200

    Add routes for places social media

--- base/urls.py
+++ base/urls.py
@@ -15,8 +15,14 @@ urlpatterns = [
     path('place/<int:pk>/delete/', deletePlace, name='deletePlace'),
 
     path('place/<int:place_id>/images/', getPlaceImages, name='getPlaceImages'),
-    path('place/<int:place_id>/images/add/', addPlaceImage, name='add_place_image'),
-    path('images/<int:pk>/', placeImageDetails, name='place_image_details'),
-    path('images/<int:pk>/edit/', editPlaceImage, name='edit_place_image'),
-    path('images/<int:pk>/delete/', deletePlaceImage, name='delete_place_image'),
+    path('place/<int:place_id>/images/add/', addPlaceImage, name='addPlaceImage'),
+    path('images/<int:pk>/', placeImageDetails, name='placeImageDetails'),
+    path('images/<int:pk>/edit/', editPlaceImage, name='editPlaceImage'),
+    path('images/<int:pk>/delete/', deletePlaceImage, name='deletePlace_Image'),
+
+    path('places/<int:place_id>/social/', getPlaceSocialMedias, name='getPlaceSocialMedias'),
+    path('places/<int:place_id>/social/add/', addPlaceSocialMedia, name='addPlaceSocialMedia'),
+    path('social/<int:pk>/', placeSocialMediaDetails, name='placeSocialMediaDetails'),
+    path('social/<int:pk>/edit/', editPlaceSocialMedia, name='editPlaceSocialMedia'),
+    path('social/<int:pk>/delete/', deletePlaceSocialMedia, name='deletePlaceSocialMedia'),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
