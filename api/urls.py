1a23a30230b8515e9cb1b60b362b87c58c490eaf
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-26 13:10:39 +0200

    Setup base app in settings

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('base.urls')),
     path('api/auth/', include('account.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
