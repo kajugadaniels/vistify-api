cf4e868bc2850172a13004639ea92631b2bf24b4
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-27 08:42:17 +0200

    Setup web app in settings

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('base.urls')),
+    path('api/', include('web.urls')),
+    path('api/admin/', include('base.urls')),
     path('api/auth/', include('account.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
