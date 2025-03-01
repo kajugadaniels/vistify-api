7f3cee208af014aa3c185c8725c91f83a2945d1e
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-01 11:30:45 +0200

    Added PlaceImageInline & PlaceSocialMediaInline to PlaceAdmin for better image & social media management

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,30 @@
 from base.models import *
 from django.contrib import admin
+from django.utils.html import format_html
+
+# -------------------
+# Place Image Inline Admin
+# -------------------
+class PlaceImageInline(admin.TabularInline):  # or admin.StackedInline for vertical layout
+    model = PlaceImage
+    extra = 1  # Allows adding one extra blank image field
+    readonly_fields = ("image_preview",)
+
+    def image_preview(self, obj):
+        """Display a small preview of the uploaded image in the admin panel."""
+        if obj.image:
+            return format_html('<img src="{}" width="100" height="60" style="border-radius:5px;" />', obj.image.url)
+        return "No Image"
+
+    image_preview.short_description = "Image Preview"
+
+# -------------------
+# Place Social Media Inline Admin
+# -------------------
+class PlaceSocialMediaInline(admin.StackedInline):
+    model = PlaceSocialMedia
+    extra = 1  # Only one social media entry per place
+    max_num = 1  # Prevents multiple social media records per place
 
 # -------------------
 # Category Admin
@@ -21,7 +46,7 @@ class TagAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
 # -------------------
-# Place Admin
+# Place Admin (with inline images & social media)
 # -------------------
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
@@ -30,6 +55,7 @@ class PlaceAdmin(admin.ModelAdmin):
     list_filter = ('category', 'province', 'district', 'created_at')
     prepopulated_fields = {'slug': ('name',)}
     filter_horizontal = ('tags',)  # Allows selecting multiple tags in a better UI
+    inlines = [PlaceImageInline, PlaceSocialMediaInline]  # Add images & social media directly
 
 # -------------------
 # Place Image Admin
@@ -41,8 +67,10 @@ class PlaceImageAdmin(admin.ModelAdmin):
     readonly_fields = ('image_preview',)
 
     def image_preview(self, obj):
-        """Display a thumbnail preview of the image in the admin panel."""
-        return obj.image_preview() if obj.image else "No Image"
+        """Display a small thumbnail preview of the image in the admin panel."""
+        if obj.image:
+            return format_html('<img src="{}" width="100" height="60" style="border-radius:5px;" />', obj.image.url)
+        return "No Image"
 
     image_preview.short_description = "Image Preview"
 
@@ -54,4 +82,3 @@ class PlaceSocialMediaAdmin(admin.ModelAdmin):
     list_display = ('place', 'phone_number', 'email', 'instagram', 'twitter', 'facebook')
     search_fields = ('place__name', 'phone_number', 'email', 'instagram', 'twitter', 'facebook')
     list_filter = ('place',)
-
